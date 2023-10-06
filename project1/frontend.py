3048ff03594d2fc2db26ed70cc9fab8a2c6d92eb
MengTianjian <mengtianjian@utexas.edu>
2023-10-06 18:45:10 +0000

    implement frontend and server

--- project1/frontend.py
+++ project1/frontend.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 import xmlrpc.client
 import xmlrpc.server
 from socketserver import ThreadingMixIn
@@ -17,14 +20,20 @@ class FrontendRPCServer:
     ## servers that are responsible for inserting a new key-value
     ## pair or updating an existing one.
     def put(self, key, value):
-        serverId = key % len(kvsServers)
-        return kvsServers[serverId].put(key, value)
+        t = time.time()
+        num_servers = len(kvsServers)
+        for serverId, rpcHandle in kvsServers.items():
+            rpcHandle.put(key, (value, t))
+        
+        # serverId = key % len(kvsServers)
+        # return kvsServers[serverId].put(key, value)
 
     ## get: This function routes requests from clients to proper
     ## servers that are responsible for getting the value
     ## associated with the given key.
     def get(self, key):
-        serverId = key % len(kvsServers)
+        # serverId = key % len(kvsServers)
+        serverId = random.randint(0, len(kvsServers) - 1)
         return kvsServers[serverId].get(key)
 
     ## printKVPairs: This function routes requests to servers
@@ -36,6 +45,11 @@ class FrontendRPCServer:
     ## serverId to the cluster membership.
     def addServer(self, serverId):
         kvsServers[serverId] = xmlrpc.client.ServerProxy(baseAddr + str(baseServerPort + serverId))
+        kv_pairs = kvsServers[random.randint(0, len(kvsServers) - 1)].printKVPairs()
+        kv_pairs = kv_pairs.split("/n")
+        for kv_pair in kv_pairs:
+            k, v = kv_pair.split(":")
+            kvsServers[serverId].put(int(k), (int(v), 0))
         return "Success"
 
     ## listServer: This function prints out a list of servers that
@@ -44,7 +58,9 @@ class FrontendRPCServer:
         serverList = []
         for serverId, rpcHandle in kvsServers.items():
             serverList.append(serverId)
-        return serverList
+        if not serverList:
+            return "ERR_NOSERVERS"
+        return ", ".join([str(serverId) for serverId in sorted(serverList)])
 
     ## shutdownServer: This function routes the shutdown request to
     ## a server matched with the specified serverId to let the corresponding
--- project1/server.py
+++ project1/server.py
@@ -7,19 +7,29 @@ basePort = 9000
 
 class KVSRPCServer:
     # TODO: You need to implement details for these functions.
+    data = dict()
 
     ## put: Insert a new-key-value pair or updates an existing
     ## one with new one if the same key already exists.
     def put(self, key, value):
-        return "[Server " + str(serverId) + "] Receive a put request: " + "Key = " + str(key) + ", Val = " + str(value)
+        if key not in self.data or value[1] > self.data[key][1]:
+            self.data[key] = value
+        # return "[Server " + str(serverId) + "] Receive a put request: " + "Key = " + str(key) + ", Val = " + str(value)
 
     ## get: Get the value associated with the given key.
     def get(self, key):
-        return "[Server " + str(serverId) + "] Receive a get request: " + "Key = " + str(key)
+        if key not in self.data:
+            return "ERR_KEY"
+        return self.data[key][0]
+        # return "[Server " + str(serverId) + "] Receive a get request: " + "Key = " + str(key)
 
     ## printKVPairs: Print all the key-value pairs at this server.
     def printKVPairs(self):
-        return "[Server " + str(serverId) + "] Receive a request printing all KV pairs stored in this server"
+        result = []
+        for k, v in self.data.items():
+            result.append('{}:{}'.format(k, v[0]))
+        return '\n'.join(result)
+        # return "[Server " + str(serverId) + "] Receive a request printing all KV pairs stored in this server"
 
     ## shutdownServer: Terminate the server itself normally.
     def shutdownServer(self):
