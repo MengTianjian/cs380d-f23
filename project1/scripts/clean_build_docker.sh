d03966756ede0484e7f88500f2f2a5188f311fb1
Ubuntu <ubuntu@master0.cluster.local>
2023-10-06 18:37:04 +0000

    update config

--- project1/scripts/clean_build_docker.sh
+++ project1/scripts/clean_build_docker.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'sekwonlee' | awk '{print $2}')
+sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'mengtianjian' | awk '{print $2}')
 
 cd dockerfiles
 
-sudo docker build . -f base.dockerfile -t sekwonlee/kvs:base --network=host
-sudo docker push sekwonlee/kvs:base
+sudo docker build . -f base.dockerfile -t mengtianjian/dist_sys_project1:base --network=host
+sudo docker push mengtianjian/dist_sys_project1:base
 
-sudo docker build . -f client.dockerfile -t sekwonlee/kvs:client --network=host
-sudo docker push sekwonlee/kvs:client
+sudo docker build . -f client.dockerfile -t mengtianjian/dist_sys_project1:client --network=host
+sudo docker push mengtianjian/dist_sys_project1:client
 
-sudo docker build . -f frontend.dockerfile -t sekwonlee/kvs:frontend --network=host
-sudo docker push sekwonlee/kvs:frontend
+sudo docker build . -f frontend.dockerfile -t mengtianjian/dist_sys_project1:frontend --network=host
+sudo docker push mengtianjian/dist_sys_project1:frontend
 
-sudo docker build . -f server.dockerfile -t sekwonlee/kvs:server --network=host
-sudo docker push sekwonlee/kvs:server
+sudo docker build . -f server.dockerfile -t mengtianjian/dist_sys_project1:server --network=host
+sudo docker push mengtianjian/dist_sys_project1:server
 
 cd ..
